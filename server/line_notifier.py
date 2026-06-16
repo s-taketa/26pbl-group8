9456d9e143b1c0ba2291ff097d2ab951881e0235
s-taketa <24110013@oic-ok.ac.jp>
2026-06-16 14:35:49 +0900

    関数の概要更新

--- server/line_notifier.py
+++ server/line_notifier.py
@@ -6,9 +6,5 @@ class LineNotifier:
         pass
 
     def sendLineNotification(self, message):
-        # LINE Messaging APIを呼び出し、異常検知や生存確認を家族へ通知する
-        pass
-
-    def sendUrgentAlert(self, message, priority):
-        # 緊急度の高い通知を送信
+        # LINE Messaging APIを呼び出し、家族へ通知する
         pass
